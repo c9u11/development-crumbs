a4e26d774f493b1a21910eaff7e94b45b8e6542a
c9u11 <tjrdud6412@naver.com>
2023-08-18 01:19:43 +0900

    🐛 README INDEX
    - pull request 정보를 확인하여 index 수정

--- util/update-readme.py
+++ util/update-readme.py
@@ -1,53 +1,38 @@
 import glob
 import os
 import time
+import sys
 from urllib import parse
 
-def make_index():
-    index = ""
-    dir_name = './md/*'
-    file_list = glob.glob(dir_name)
-    file_list.sort(key=os.path.getmtime, reverse=True)
-    for file_path in file_list:
-        timestamp_str = time.strftime(  '%Y/%m/%d %H:%M:%S', time.gmtime(os.path.getmtime(file_path)))
-        file_name = file_path[5:-3]
-        file_link = f'''https://github.com/c9u11/development-crumbs/blob/main/{parse.quote(file_path[2:])}'''
-        index += f'''{timestamp_str} - [{file_name}]({file_link})\n\n'''
-    
+README_FILE_PATH = "./README.md"
+INDEX_STRING = '## 부스러기 목차\n'
+
+
+def format_index(pr):
+    index = INDEX_STRING
+    file_link = f'''https://github.com/c9u11/development-crumbs/blob/main/{pr['title']}'''
+    index += f'''{pr['datetime']} - [{pr['title']}]({file_link}) : {pr['user']}\n\n'''
+
     return index
 
 
-def make_read_me(index):
-    return f"""
-# 개발 부스러기
-
-남들이 먹고 남긴 개발 부스러기를 주워 먹읍시다.
-
+def get_readme_md():
+    with open(README_FILE_PATH, 'r', encoding='utf-8') as f:
+        readme = f.read()
+    return readme
 
 
-## 부스러기 목차
-{index}
+def update_readme_md(new):
+    with open(README_FILE_PATH, 'w') as file:
+        file.write(new)
 
-
-
-## [부스러기 나눔](https://github.com/c9u11/development-crumbs/blob/main/md/%EA%B0%9C%EB%B0%9C%20%EB%B6%80%EC%8A%A4%EB%9F%AC%EA%B8%B0%20%EB%82%98%EB%88%94%20%EB%B0%A9%EB%B2%95.md)
-
-먹다 남긴 개발 부스러기가 있다면 나눠주세요.
-
-어떠한 향과 맛 상관없습니다. 감사합니다.
-
-1. 포장지를 받습니다. (Fork the Project)
-2. 개발 부스러기를 모읍니다. (Write .md File)
-3. 개발 부스러기를 포장합니다. (Commit your Changes)
-4. 포장한 부스러기를 문 앞에 놓으세요. (Push to the Branch)
-5. 나눔 신청을 통해 모두에게 나누어주세요. (Open a Pull Request)
-"""
-
-
-def update_readme_md():
-    readme = make_read_me(make_index())
-    with open("./README.md", 'w', encoding='utf-8') as f:
-        f.write(readme)
 
 if __name__ == "__main__":
-    update_readme_md()
+    pr = {
+        'title': sys.argv[1],
+        'datetime': sys.argv[2],
+        'user': sys.argv[3]
+    }
+    old_readme = get_readme_md()
+    new_readme = old_readme.replace(INDEX_STRING, format_index(pr))
+    update_readme_md(new_readme)
