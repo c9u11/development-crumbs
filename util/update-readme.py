bb4b9ccd171e1dd82bf8ff6e23d5685939a76496
HWANG SEOKYOUNG <tjrdud6412@naver.com>
2023-08-18 16:23:07 +0900

    Update update-readme.py

--- util/update-readme.py
+++ util/update-readme.py
@@ -9,7 +9,7 @@ INDEX_STRING = '## 부스러기 목차\n'
 
 def format_index(pr):
     index = INDEX_STRING
-    file_link = f'''https://github.com/c9u11/development-crumbs/blob/main/{parse.quote(pr['title'])}.md'''
+    file_link = f'''https://github.com/c9u11/development-crumbs/blob/main/md/{parse.quote(pr['title'])}.md'''
     index += f'''{pr['datetime']} - [{pr['title']}]({file_link}) : {pr['user']}\n\n'''
     return index
 
